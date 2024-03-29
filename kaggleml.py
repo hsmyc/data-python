b49532706f06dce4149244e1c7def980773d8de5
hsmyc <hllsmtyc@gmail.com>
2024-03-29 05:43:43 +0100

    Changes to be committed: 	new file:   C1_W3_Lab09_Regularization_Soln.ipynb 	new file:   C1_W3_Logistic_Regression.ipynb 	new file:   House_Sales_in_King_Count_USA.ipynb 	new file:   housing.csv 	modified:   kaggleml.py 	new file:   test.csv 	new file:   train.csv

--- kaggleml.py
+++ kaggleml.py
@@ -1,20 +1,16 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
-
+from sklearn.metrics import mean_absolute_error
 melbourne_file_path = 'melb_data.csv'
 melbourne_data = pd.read_csv(melbourne_file_path)
-melbourne_data.columns()
-print(melbourne_data.describe())  # Show summary statistics
+melbourne_data = melbourne_data.dropna(axis=0)
 y = melbourne_data.Price  # Selecting the prediction target
-melbourne_features = ['Rooms', 'Bathroom',
-                      'Landsize', 'Lattitude', 'Longtitude']
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea',
+                      'YearBuilt', 'Lattitude', 'Longtitude']
 X = melbourne_data[melbourne_features]  # Selecting the features
-print(X.describe())
-print(X.head())
+
 
 melbourne_model = DecisionTreeRegressor(random_state=1)
 melbourne_model.fit(X, y)
-print("Making predictions for the following 5 houses:")
-print(X.head())
-print("The predictions are")
-print(melbourne_model.predict(X.head()))
+predicted_home_prices = melbourne_model.predict(X)
+print(mean_absolute_error(y, predicted_home_prices))
